82ce54b79de7f5ae5ab79b23a49455c2e85d17c3
Loic Fremont <lc.fremont@gmail.com>
2019-07-31 00:21:07 +0200

    theme repository

---
+++ src/Repository/ThemeRepository.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of DeadOrAliveQuizz.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+
+class ThemeRepository extends EntityRepository
+{
+    public function createListQueryBuilder(string $localeCode)
+    {
+        $queryBuilder = $this->createQueryBuilder('o');
+        $queryBuilder
+            ->addSelect('translation')
+            ->leftJoin('o.translations', 'translation')
+            ->andWhere('translation.locale = :localeCode')
+            ->setParameter('localeCode', $localeCode);
+
+        return $queryBuilder;
+    }
+}
