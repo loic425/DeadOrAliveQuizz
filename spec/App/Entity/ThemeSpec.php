403390fb8ba45b68daf077268740385edcb16dfd
Loïc Frémont <loic@mobizel.com>
2019-07-31 14:53:05 +0200

    improve spec of theme

--- spec/App/Entity/ThemeSpec.php
+++ spec/App/Entity/ThemeSpec.php
@@ -11,6 +11,12 @@ use Sylius\Component\Resource\Model\TranslatableInterface;
 
 class ThemeSpec extends ObjectBehavior
 {
+    public function let()
+    {
+        $this->setCurrentLocale('en_US');
+        $this->setFallbackLocale('en_US');
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Theme::class);
@@ -29,5 +35,17 @@ class ThemeSpec extends ObjectBehavior
     function it_initializes_translation_collection_by_default(): void
     {
         $this->getTranslations()->shouldHaveType(Collection::class);
+    }
+
+    function it_has_no_name_by_default(): void
+    {
+        $this->getName()->shouldReturn(null);
+    }
+
+    function its_name_is_mutable(): void
+    {
+        $this->setName('Cinema');
+
+        $this->getName()->shouldReturn('Cinema');
     }
 }
