d546bf21b8a00dd86089ce3086ca2c9ec5c391dd
Loïc Frémont <loic@mobizel.com>
2019-08-01 11:17:44 +0200

    Game session endpoint

--- tests/Controller/GameSession/GameSessionCreateApiTest.php
+++ tests/Controller/GameSession/GameSessionCreateApiTest.php
@@ -14,23 +14,30 @@ declare(strict_types=1);
 namespace App\Tests\Controller\GameSession;
 
 use App\Entity\AppUserInterface;
+use App\Tests\AuthorizedHeaderTrait;
 use App\Tests\Controller\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class GameSessionCreateApiTest extends JsonApiTestCase
 {
+    use AuthorizedHeaderTrait;
+
     /**
      * @test
      */
     public function it_creates_game_session(): void
     {
         $resources = $this->loadFixturesFromFiles([
-            'authentication/api_user',
+            'authentication/api_user.yaml',
             'users.yaml',
         ]);
 
         $user = $resources['challenged_user'];
 
-        $this->client->request('POST', $this->getUserUrl($user).'/game-sessions/', [], []);
+        $this->client->request('POST', $this->getUserUrl($user).'/game_sessions', [], [], self::$authorizedHeaderWithContentType);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'game_session/create_response', Response::HTTP_CREATED);
     }
 
     private function getUserUrl(AppUserInterface $user): string
