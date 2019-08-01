5fd0db759c3608dfbaab2152ad4dff7a4b04004e
Loic Fremont <lc.fremont@gmail.com>
2019-08-01 15:26:50 +0200

    Round create endpoint

---
+++ src/Form/Type/RoundType.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of DeadOrAliveQuizz.
+ *
+ * (c) Mobizel
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use App\Entity\Round;
+use App\Entity\Theme;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RoundType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder
+            ->add('theme', EntityType::class, [
+                'class' => Theme::class,
+                'label' => 'app.ui.theme',
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Round::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'app_round';
+    }
+}
